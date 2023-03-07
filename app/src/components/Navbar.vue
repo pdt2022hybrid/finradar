0f63e68ccbb0bd37963aa71ec7e9db5d70881502
Hidas <115142767+HidasD@users.noreply.github.com>
2023-03-07 12:00:26 +0100

    rename

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -8,7 +8,6 @@
     <div class=" mr-40">
       <input type="text" class="mr-24 bg-slate-500 text-white">
       <router-link to="/register" class="text-white">user space</router-link>
-
     </div>
   </nav>
 </template>
--- app/src/views/UserDashboard.vue
+++ app/src/views/UserDashboard.vue
@@ -1,9 +1,10 @@
 <template>
+
 </template>
 
 <script>
 export default {
-  name: "DashPage",
+  name: "UserDashboard",
 }
 </script>
 
