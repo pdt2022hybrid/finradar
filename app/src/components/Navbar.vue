96bdb869d1de9d7adf9c3c9ad8e0ff4d2ef79e38
raven75xd <115644871+raven75xd@users.noreply.github.com>
2023-03-07 01:30:45 -0800

    tlosku tailwindu na navbare

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -1,12 +1,13 @@
 <template>
-  <nav>
-    <div>
-      <router-link to="/">logo space</router-link>
+  <nav class="flex place-content-between">
+    <div class=" ml-40">
+      <router-link class=" mr-24" to="/">logo space</router-link>
       <router-link to="/databazy">databazy</router-link>
     </div>
-    <div>
+    <div class=" mr-40">
       <input type="text" disabled>
       <router-link to="/register">user space</router-link>
+
     </div>
   </nav>
 </template>
