9f55841d56a2cee0db179e1f539e52036597009a
raven75xd <115644871+raven75xd@users.noreply.github.com>
2023-03-07 01:55:29 -0800

    navbar kinda done ig

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -1,12 +1,13 @@
 <template>
-  <nav class="flex place-content-between">
+  <nav class="flex place-content-between bg-slate-700">
     <div class=" ml-40">
-      <router-link class=" mr-24" to="/">logo space</router-link>
-      <router-link to="/databazy">databazy</router-link>
+      <router-link class=" mr-24 text-white" to="/">logo space</router-link>
+      <router-link to="/databazy" class="text-white mr-24">databazy</router-link>
+      <router-link to="/" class="text-white">API</router-link>
     </div>
     <div class=" mr-40">
-      <input type="text" disabled>
-      <router-link to="/register">user space</router-link>
+      <input type="text" class="mr-24" disabled>
+      <router-link to="/register" class="text-white">user space</router-link>
 
     </div>
   </nav>
