da51fb5fae68681c7f079ae798d065128b190dd6
Hidas <115142767+HidasD@users.noreply.github.com>
2023-03-07 13:29:59 +0100

    footer- cuss

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class=" bg-dark w-full bottom-0 py-4">
-    <p class="text-light place-content-center flex"> kokot </p>
+    <p class="text-navtext place-content-center flex"> footer </p>
   </div>
 </template>
 
