ce719d9e8998c0b6fc4845cde81a82ac445874a4
Hidas <115142767+HidasD@users.noreply.github.com>
2023-03-07 09:04:22 +0100

    VUE setup
    - Basic komponenty
    - Router + Tailwind setup

---
+++ app/src/router/index.ts
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: () => import('@/views/HomePage.vue')
+    },
+    {
+      path: '/databazy',
+      name: 'Databazy',
+      component: () => import('@/views/Databazy.vue')
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/DashPage.vue')
+    },
+    {
+      path: '/company',
+      name: 'Company',
+      component: () => import('@/views/CompanyPage.vue')
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: () => import('@/views/RegPage.vue')
+    }
+  ]
+})
+
+export default router
---
+++ app/src/views/DashPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "DashPage"
+}
+</script>
+
+<style scoped>
+
+</style>
