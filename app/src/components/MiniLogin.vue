5dff0c7d4cd411d608503a9669c866598d8dd947
Hidas <115142767+HidasD@users.noreply.github.com>
2023-03-08 08:43:14 +0100

    mini login screen basic

---
+++ app/src/components/MiniLogin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-light flex w-1/6 h-1/3 justify-center fixed right-32 border-2 rounded-xl p-2 mt-2">
+    <h1>Log In</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiniLogin"
+}
+</script>
+
+<style scoped>
+
+</style>
