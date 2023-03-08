98e32d65656494183136e6d1221b54363b8accf4
Hidas <115142767+HidasD@users.noreply.github.com>
2023-03-08 09:14:13 +0100

    mini login screen basic

--- app/src/components/MiniLogin.vue
+++ app/src/components/MiniLogin.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="bg-light flex w-1/6 h-1/3 justify-center fixed right-32 border-2 rounded-xl p-2 mt-2">
-    <h1>Log In</h1>
-  </div>
+  <form id="Login" class="bg-light flex w-1/6 items-center fixed right-32 border-2 rounded-xl p-5 mt-2 flex-col">
+    <h1 class="text-4xl">Log In</h1>
+    <input type="text" class="inputs mt-4 rounded" placeholder="mail">
+    <input type="text" class="inputs rounded" placeholder="pass">
+    <div class="flex items-center">
+      <input type="checkbox" class="inputs">
+      <p class="ml-4"> Remember me </p>
+    </div>
+    <button type="submit" form="Login" value="Submit"
+    class="bg-green border p-1"
+    >Submit</button>
+    <div class="flex justify-around">
+      <a href="/register">Sign up</a>
+      <a href="/"> Forgot your password?</a>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -11,5 +24,14 @@ export default {
 </script>
 
 <style scoped>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 
+@layer base {
+
+  .inputs {
+    @apply bg-background h-1/4 my-2.5 py-1 px-3
+  }
+}
 </style>
