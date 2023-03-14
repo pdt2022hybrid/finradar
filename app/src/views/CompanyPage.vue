70c6987fafa43e70460af90a93962c1076c981de
raven75xd <115644871+raven75xd@users.noreply.github.com>
2023-03-14 04:38:16 -0700

    zacal som s company

--- app/src/views/CompanyPage.vue
+++ app/src/views/CompanyPage.vue
@@ -1,9 +1,39 @@
 <template>
+
+  <div class="flex flex-col items-center border-2 p-20 m-20">
+    <div class="flex flex-col items-start ">
+    </div>
+    <div class="grid-cols-1 grid-flow-col ">
+      <h3>
+        Vznik
+      </h3>
+      <h3>
+        Sídlo
+      </h3>
+    </div>
+  </div>
+
 </template>
 
 <script >
+import axios from 'axios';
 export default {
   name: "CompanyPage",
+data() {
+    return {
+      Data: [],
+    }
+  },
+  mounted() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response)=> {
+        console.log(response);
+      }) 
+      .catch((errors)=> {
+        console.log(errors);
+      })
+  }
 }
 </script>
 
