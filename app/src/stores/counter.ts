e1d1864338d30b3bb026242a8cd143dc19150453
Hidas <115142767+HidasD@users.noreply.github.com>
2023-03-14 10:00:13 +0100

    niake mini changes

--- app/src/stores/counter.ts
+++ app/src/stores/counter.ts
@@ -2,3 +2,22 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 // ma z tohto jebne pre pana jana
+
+interface State {
+    UserList: UserData[]
+    user: UserData | null
+
+}
+export const useUserStore = defineStore('LoggedIn', {
+    state: (): State => {
+        return {
+            UserList: [],
+            user: null,
+        }
+    },
+})
+
+interface UserData {
+    id: string
+    name: string
+}
--- app/src/views/Databazy.vue
+++ app/src/views/Databazy.vue
@@ -1,9 +1,53 @@
 <template>
+  <div class="flex justify-center">
+    <div>
+      <div>
+
+      </div>
+      <table class="w-4/5 bg-light">
+        <thead class="border-2">
+        <tr>
+          <th>Meno</th>
+          <th>Trzby</th>
+          <th>Zisk</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in this.Data">
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Databazy",
+  data() {
+    return {
+      Data: [],
+    }
+  },
+  // funkcia, ktora sa spusti ked sa loadne page
+  mounted() {
+    axios
+        // z tohto ulr to bere data a da to do response ako promise
+        .get('https://jsonplaceholder.typicode.com/todos/1')
+        // tunak manipulujeme s responsom a dame ho do premennej Data
+        .then((response) => {
+          console.log(response);
+          this.Data = response;
+          })
+        // tunak ak je error tak ho to zachity a vypise
+        .catch((errors) => {
+          console.log(errors); // Errors
+        });
+  }
 }
 </script>
 
@@ -11,4 +55,10 @@ export default {
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
+
+@layer base {
+  tr > th, tr > td  {
+    @apply border-2
+  }
+}
 </style>
