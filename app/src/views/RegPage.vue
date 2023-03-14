49d020fa9a07926e3d15a180d38af815bb0d6673
raven75xd <115644871+raven75xd@users.noreply.github.com>
2023-03-14 02:03:59 -0700

    reg

--- app/src/views/RegPage.vue
+++ app/src/views/RegPage.vue
@@ -4,18 +4,18 @@
     <h1 class=" text-4xl font-bold">Registration</h1>
     <h2 class=" mb-16 text-xl pt-14 font-semibold">Enter your information here to register an account</h2>
       <div class=" mb-48 mr-96 ml-96">
-        <div class=" bg-light ml-48 mr-48 p-5 scale-110">
+        <div class=" bg- ml-48 mr-48 p-5 scale-110">
           <div class="pl-2 pr-2 m-1">
-          <p class="text-left text-sm">Full name</p>
-          <input type="text" class="text-center text-dark w-56 rounded">
-          <p class="text-left text-sm">E-mail</p>
-          <input type="text" class="text-center text-dark w-56 rounded">
-          <p class="text-left text-sm">Password (min. 8 characters)</p>
-          <input type="text" class="text-center text-dark w-56 rounded">
-          <p class="text-left text-sm">Confirm password</p>
-          <input type="text" class="text-center w-56 text-dark rounded">
+          <p class="text-left text-sm mb-1">Full name</p>
+          <input type="text" class="bar">
+          <p class="text-left text-sm mb-1">E-mail</p>
+          <input type="text" class="bar">
+          <p class="text-left text-sm mb-1">Password (min. 8 characters)</p>
+          <input type="text" class="bar">
+          <p class="text-left text-sm mb-1">Confirm password</p>
+          <input type="text" class="bar">
           </div>
-          <button class="bg-green rounded mt-4 w-24 text-dark scale-125">Submit</button>
+          <button class="bg-green rounded mt-4 w-56 text-dark">Submit</button>
         </div>
       </div>
   </div>
@@ -30,4 +30,11 @@ export default {
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
+
+@layer base {
+  .bar {
+    @apply pl-1 mb-1 w-56 text-dark rounded
+  }
+}
+
 </style>
