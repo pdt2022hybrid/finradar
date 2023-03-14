856578ea7476644a9221f51a682dcb9538fd1648
alegzzz <Alex.Szabo@spsehalova.sk>
2023-03-14 12:22:08 +0100

    regpage update

--- app/src/views/RegPage.vue
+++ app/src/views/RegPage.vue
@@ -1,48 +1,48 @@
 <template>
   <!-- mi je zle z tohto tailwindu -->
-  <div class=" text-center pt-12">
-    <h1 class=" text-4xl font-bold">Registration</h1>
-    <h2 class=" mb-16 text-xl pt-14 font-semibold">Enter your information here to register an account</h2>
-      <div class="mb-48 mx-48">
-        <div class=" justify-center bg-tables mx-96 p-6">
+  <div class="mb-48 place-content-center flex ">
+   <div class=" text-center pt-12 flex-row">
+    <h1 class=" text-4xl font-bold">Registrácia</h1>
+    <h2 class=" mb-16 text-xl pt-14 font-semibold">Sem zadajte Vaše informácie na registráciu profilu</h2>
+        <form class=" bg-tables p-6 flex-row">
           <label class="block">
           <span class="after:ml-0.5 after:text-red-500 block text-sm text-left font-medium text-slate-700">
-            Full name
+            Meno a Priezvisko
             </span>
-            <input 
-              type="name" 
-              name="full name" 
-              class="mt-1 px-3 py-2 bg-white shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" 
-              placeholder="your name and last name" />
+            <input
+              type="name"
+              name="full name"
+              class="mt-1 px-3 py-2 bg-white shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+              placeholder="Vaše meno a priezvisko">
           </label>
           <label class="block">
           <span class="after:ml-0.5 after:text-red-500 block text-sm text-left font-medium text-slate-700">
-            Email
+            E-mail
             </span>
-            <input 
-              type="email" 
-              name="email" 
-              class="mt-1 px-3 py-2 bg-white shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" 
+            <input
+              type="email"
+              name="email"
+              class="mt-1 px-3 py-2 bg-white shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
               placeholder="you@example.com" />
           </label>
           <label class="block">
           <span class="after:ml-0.5 after:text-red-500 block text-sm text-left font-medium text-slate-700">
-            Password
+            <span>Heslo (min. 8 znakov)</span>
             </span>
-            <input 
-              type="password" 
-              name="passw" 
-              class="mt-1 px-3 py-2 bg-white shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" 
-              placeholder="(min 8 characters)" />
+            <input
+              type="password"
+              name="passw"
+              class="mt-1 px-3 py-2 bg-white shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+              placeholder="Vaše heslo" />
           </label>
           <label class="block">
           <span class="after:ml-0.5 after:text-red-500 block text-sm text-left font-medium text-slate-700">
-            Confirm password
+            Potvrdenie Hesla
             </span>
-            <input 
-              type="confirm pass" 
-              name="confpass" 
-              class="mt-1 px-3 py-2 bg-white shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" 
+            <input
+              type="confirm pass"
+              name="confpass"
+              class="mt-1 px-3 py-2 bg-white shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
               placeholder="" />
           </label>
           <!-- <div class=""> -->
@@ -56,8 +56,8 @@
           <input type="text" class="bar">
           <button class="bg-green rounded mt-4 w-64 text-dark h-7">Submit</button>
           </div> -->
-          <button class="bg-green rounded mt-6 w-full text-dark h-9">Submit</button>
-        </div>
+          <button class="bg-green rounded mt-6 w-full text-dark h-9">Potvrdiť	</button>
+        </form>
       </div>
   </div>
 </template>
