25378c05317e361f906a9a22ad47f7d921fd1ce1
Hidas <115142767+HidasD@users.noreply.github.com>
2023-03-15 09:40:59 +0100

    fixed text indent in user dropdown menu

--- app/src/components/UserMiniSettings.vue
+++ app/src/components/UserMiniSettings.vue
@@ -7,16 +7,16 @@
      <header class="font-rubik font-medium text-lg border-b-2"> Smigus Dyngus </header>
      <router-link to="/dashboard" class="border-b-2 pt-2 pb-2 font-normal flex">
        <i class="bi bi-bar-chart-line-fill  text-green"></i>
-		<p>Dashboard</p>
-    </router-link>
-	<router-link to="/" class="border-b-2 pt-2 pb-2 flex">
-        <i class="bi bi-gear-fill text-green"></i>
-       	<p class="ml-3">Settings</p>
-	</router-link>
-    <router-link to="/" class="border-b-2 pt-2 pb-2 flex">
-          <i class="bi bi-box-arrow-right text-green"></i>
-       		<p>Log Out</p>
-	 </router-link>
+       <p class="indent-3">Dashboard</p>
+     </router-link>
+	   <router-link to="/" class="border-b-2 pt-2 pb-2 flex">
+       <i class="bi bi-gear-fill text-green"></i>
+       <p class="indent-3">Settings</p>
+	   </router-link>
+     <router-link to="/" class="border-b-2 pt-2 pb-2 flex">
+       <i class="bi bi-box-arrow-right text-green"></i>
+       <p class="indent-3">Log Out</p>
+	   </router-link>
    </div>
 </template>
 
