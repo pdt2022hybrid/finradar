edf41dad629be433bcb1ae588dfe0c4b7b7486dd
Hidas <115142767+HidasD@users.noreply.github.com>
2023-03-21 11:15:14 +0100

    Commit - databazy funguju celkom

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import axios from "axios"
+// @ts-ignore
 import VueChartkick from 'vue-chartkick'
 import 'chartkick/chart.js'
 
--- app/src/views/Databazy.vue
+++ app/src/views/Databazy.vue
@@ -1,43 +1,50 @@
 <template>
-	<div class="">
+	<div>
     <div class="flex-col ml-52 mt-16 flex items-start font-rubik">
       <h1 class="text-4xl mb-10 font-extrabold"> Databázy </h1>
       <h3 class="text-xl font-bold mb-4 indent-8"> Filtrovanie v databáze</h3>
     </div>
-	<div class="flex flex-col items-center">
-    <div class="flex-col border-x border-t flex w-3/4 mb-28">
-      <form class="bg-tables flex flex-row p-6">
-        <div class="w-1/2 grid">
-          <input type="text" class="label placeholder-dark placeholder:font-rubik" placeholder="Meno / IČO firmy">
-          <span class="inline-grid grid-cols-2">
-            <input class="label placeholder-dark placeholder:font-rubik" type="number" placeholder="Tržby od">
-            <input class="label placeholder-dark placeholder:font-rubik" type="number" placeholder="Tržby do">
-            <input class="label placeholder-dark placeholder:font-rubik" type="number" placeholder="Zisk od">
-            <input class="label placeholder-dark placeholder:font-rubik" type="number" placeholder="Zisk do">
-          </span>
-        </div>
-        <div class="w-1/2 flex justify-end items-end">
-          <button class="w-3/5 bg-blue p-1.5 rounded border font-varela">Hľadaj</button>
-        </div>
-      </form>
-      <table class="bg-tables w-full font-varela">
-        <thead class="border-y bg-dark text-background">
-        <tr>
-          <th class="border-r">Názov</th>
-          <th class="border-r">Tržby</th>
-          <th class="">Zisk</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="item in this.Data.data">
-          <td class="border-r"> {{ item.name }}</td>
-          <td class="border-r text-center"> {{ item.latest_data.capital }} €</td>
-          <td class="text-center"> {{ item.latest_data.revenue }} €</td>
-        </tr>
-        </tbody>
-      </table>
+    <div class="flex flex-col items-center">
+      <div class="flex-col border-x border-t flex w-3/4 mb-28">
+        <form class="bg-tables flex flex-row p-6" >
+          <div class="w-1/2 grid">
+            <input type="text" class="label placeholder-dark placeholder:font-rubik" placeholder="Meno / IČO firmy">
+            <span class="inline-grid grid-cols-2">
+              <input class="label placeholder-dark placeholder:font-rubik" type="number" placeholder="Tržby od">
+              <input class="label placeholder-dark placeholder:font-rubik" type="number" placeholder="Tržby do">
+              <input class="label placeholder-dark placeholder:font-rubik" type="number" placeholder="Zisk od">
+              <input class="label placeholder-dark placeholder:font-rubik" type="number" placeholder="Zisk do">
+            </span>
+          </div>
+          <div class="w-1/2 flex justify-end items-end">
+            <button class="w-3/5 bg-blue p-1.5 rounded border font-varela">Hľadaj</button>
+          </div>
+        </form>
+        <table class="bg-tables w-full font-varela">
+          <thead class="border-y bg-dark">
+          <tr>
+            <th class="border-r"> <h4 class="text-background">Názov</h4>
+            </th>
+            <th class="border-r"> <h4 class="text-background">Tržby</h4>
+            </th>
+            <th> <h4 class="text-background">Zisk</h4>
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="item in this.Data">
+            <td class="border-r">
+              <h4 class="cursor-pointer" @click="$router.go(-1)"> {{ item.name }} </h4>
+            </td>
+            <td class="border-r text-center"> {{ item.latest_data.capital }} €</td>
+            <td class="text-center"> {{ item.latest_data.revenue }} €</td>
+          </tr>
+          </tbody>
+          <tfoot>
+          </tfoot>
+        </table>
+      </div>
     </div>
-	</div>
 	</div>
 </template>
 
@@ -51,18 +58,16 @@ export default {
     }
   },
   // funkcia, ktora sa spusti ked sa loadne page
-  mounted() {
-    axios
-        // z tohto ulr to bere data a da to do response ako promise
-        .get('/api/companies')
-        // tunak manipulujeme s responsom a dame ho do premennej Data
-        .then((response) => {
-          console.log(response);
-          this.Data = response.data;
+  async mounted() {
+    try {
+      await axios.get('/api/companies')
+          .then((response) => {
+            console.log(response);
+            this.Data = response.data.data;
           })
-        .catch((errors) => {
-          console.log(errors); // Errors
-        });
+    } catch(errors) {
+      console.log(errors);
+    }
   }
 }
 </script>
