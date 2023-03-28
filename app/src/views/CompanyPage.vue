92f227dae726449140d2408108559416867cd835
raven75xd <115644871+raven75xd@users.noreply.github.com>
2023-03-28 01:41:01 -0700

    company

--- app/src/views/CompanyPage.vue
+++ app/src/views/CompanyPage.vue
@@ -2,10 +2,6 @@
 <div class=" flex justify-center pt-10">
   <div class="border-2 bg-tables grid-cols-3 grid w-3/4">
     <div class="flex flex-col items-start p-6">
-      <table>
-        {{ this.Data.latest_data.full_report.assets.financial_accounts_total
- }}
-      </table>
       <h3 class="border-b">Vznik</h3>
       <h3 class="border-b">Sídlo</h3>
       <h3 class="border-b">CEO</h3>
@@ -17,7 +13,7 @@
         <h1 class="">Tržby</h1>
         <line-chart :data="{'2017-05-13': 2, '2017-05-14': 5}"></line-chart>
         <h1>Aktíva</h1>
-        <pie-chart :data="[['Blueberry', this.Data.latest_data.full_report.assets.financial_accounts_total], ['Strawberry', 23]]"></pie-chart>
+        <pie-chart :data="[['Blueberry', this.Data.latest_data.full_report.assets.financial_accounts_total ], ['Strawberry', 23]]"></pie-chart>
        <!--- <div class="flex flex-col text-left pt-10">
         <h2 class="">
           Broker Service Group ma 25% ^ zisk
@@ -38,7 +34,7 @@
         <h1>Zisky</h1>
         <line-chart :data="{'2017-05-13': 2, '2017-05-14': 5}"></line-chart>
         <h1>Pasíva</h1>
-        <pie-chart :data="[['Blueberry', 4], ['Strawberry', 10 ]]"></pie-chart>
+        <pie-chart :data="[['Blueberry', 90 ], ['Strawberry', 10 ]]"></pie-chart>
       </div>
   </div>
   </div>
