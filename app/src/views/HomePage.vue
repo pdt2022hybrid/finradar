d18f2f9428c6dda7d89d38d83c85eb821482b497
alegzzz <Alex.Szabo@spsehalova.sk>
2023-04-04 11:38:50 +0200

    homepage update

--- app/src/views/HomePage.vue
+++ app/src/views/HomePage.vue
@@ -3,15 +3,15 @@
 <div class="wrapper">
 	<header class="homepageHeader">
 		<h1 class="header-text">Vyhľadávanie v Databáze Firiem</h1>
-		<form onsubmit="return false">
+		<form onsubmit="return false lg:fixed">
       <input type="text" class="header-btn placeholder-white font-varela" v-model="name" placeholder="Vyhľadať firmu" @keyup="Search">
-	  <i class="bi bi-search text-sm absolute right-1/2 mr-16 top-2/4 mt-24 pt-9 text-white"></i>
+	  <i class="bi bi-search lg:text-sm absolute right-1/2 mr-16 top-2/4 mt-24 pt-9 text-white"></i>
       <button class="hidden" @click="Search"></button>
     </form>
     <ul>
       <li v-for="item in this.Data">
         <router-link :to="{ name: 'company', params: { ico: item.ico } }" v-slot="{ redirect }">
-          <h4 class="cursor-pointer w-fit" @click="redirect"> {{ item.name }} </h4>
+          <h4 class="cursor-pointer lg:w-fit" @click="redirect"> {{ item.name }} </h4>
         </router-link>
       </li>
     </ul>
@@ -87,36 +87,37 @@ export default {
 @layer base {
 
 	.wrapper{
-		@apply min-h-screen bg-background
+		@apply
+				lg:min-h-screen bg-background
 	}
 
 	.homepageHeader{
-		@apply  items-center text-center w-full pt-44
+		@apply  lg:items-center text-center w-full pt-44
 	}
 
 	.header-text{
-		@apply block font-rubik font-bold text-dark text-5xl
+		@apply lg:block font-rubik font-bold text-dark text-5xl
 
 	}
 
 	.header-btn{
-		@apply text-center items-center mt-20 mb-36 w-64 h-10 rounded-lg bg-blue
+		@apply lg:text-center items-center mt-20 mb-36 w-64 h-10 rounded-lg bg-blue
 	}
 
 	.homepageMainContent{
-		@apply flex place-content-evenly h-fit py-20 pb-20 pb-40 bg-light
+		@apply lg:flex place-content-evenly h-fit py-20 pb-20 pb-40 bg-light
 	}
 
 	.mainContentText{
-		@apply flex w-96 flex-col text-xl text-dark font-normal
+		@apply lg:flex w-96 flex-col text-xl text-dark font-normal
 	}
 
 	.mainContentText h2{
-		@apply mb-10 text-center text-3xl font-rubik text-dark font-semibold
+		@apply lg:mb-10 text-center text-3xl font-rubik text-dark font-semibold
 	}
 
 	.mainContentImage{
-		@apply w-32 h-32 flex border-2 mt-40
+		@apply lg:w-32 h-32 flex border-2 mt-40
 	}
 
 }
