f5f46a7d624ec540395400b22fbac55693b97468
Ivan Polák <ivan.polak25@gmail.com>
2023-05-08 19:44:45 +0200

    [fe] formatting

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,36 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
-
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
         extend: {},
         colors: {
-            'green': '#00ff00',
-            'purp': '#A020F0',
-            'red': '#FF0000',
-            'yell': '#FFA500',
-            'tables': '#EFEFEF',
-            'white': '#FFFFFF',
-            'background': '#C8C8C8',
-            'dark': '#434040',
-            'blue': '#3399E9',
-            'blue_light': '#83C2F0',
-            'light': '#D9D9D9',
-            'search': '#DEDEDE',
-            'navtext': '#CFCFCF',
-            'underline': '#3399E9',
+            green: "#00ff00",
+            purp: "#A020F0",
+            red: "#FF0000",
+            yell: "#FFA500",
+            tables: "#EFEFEF",
+            white: "#FFFFFF",
+            background: "#C8C8C8",
+            dark: "#434040",
+            blue: "#3399E9",
+            blue_light: "#83C2F0",
+            light: "#D9D9D9",
+            search: "#DEDEDE",
+            navtext: "#CFCFCF",
+            underline: "#3399E9",
         },
-
         fontFamily: {
-            'rubik': ['"Rubik"', ...defaultTheme.fontFamily.sans],
-            'varela': ['"Varela"', ...defaultTheme.fontFamily.sans],
-
-        }
+            rubik: ['"Rubik"', ...defaultTheme.fontFamily.sans],
+            varela: ['"Varela"', ...defaultTheme.fontFamily.sans],
+        },
     },
     plugins: [],
-}
+};
