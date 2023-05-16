66d2d70ab0d7c8f7e4a07e7c6db7be482d1ac5c6
Hidas <115142767+HidasD@users.noreply.github.com>
2023-05-16 13:27:48 +0200

    Commit - register logika

--- app/src/views/RegPage.vue
+++ app/src/views/RegPage.vue
@@ -1,5 +1,4 @@
 <template>
-    <!-- mi je zle z tohto tailwindu -->
     <div class="items-center flex flex-col lg:mb-20">
         <div class="lg:w-1/2 flex-col flex items-center w-full">
             <h1 class="lg:mt-16 text-4xl font-bold lg:w-3/4 mt-32 text-center">
@@ -12,7 +11,7 @@
             </h2>
         </div>
         <div class="flex justify-center lg:w-1/2">
-            <form class="bg-search p-6 px-16 lg:w-3/4 rounded-md">
+            <form class="bg-search p-6 px-16 lg:w-3/4 rounded-md" onsubmit="return false">
                 <label class="block">
                     <span
                         class="after:ml-0.5 after:text-red-500 block text-sm text-left font-medium text-slate-700 mb-2"
@@ -20,10 +19,11 @@
                         Meno a Priezvisko
                     </span>
                     <input
-                        type="name"
+                        type="text"
                         name="full name"
                         class="mt-1 px-3 py-2 bg-background shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:ring-green block w-full rounded-md sm:text-sm focus:ring-1 focus:placeholder:text-background"
                         placeholder="Vaše meno a priezvisko"
+                        v-model="fullName"
                     />
                 </label>
                 <label class="block">
@@ -37,6 +37,7 @@
                         name="email"
                         class="mt-1 px-3 py-2 bg-background shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:ring-green block w-full rounded-md sm:text-sm focus:ring-1 focus:placeholder:text-background"
                         placeholder="meno@priklad.com"
+                        v-model="email"
                     />
                 </label>
                 <label class="block">
@@ -50,6 +51,7 @@
                         name="passw"
                         class="mt-1 px-3 py-2 bg-background shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:ring-green block w-full rounded-md sm:text-sm focus:ring-1 focus:placeholder:text-background"
                         placeholder="Vaše heslo"
+                        v-model="password"
                     />
                 </label>
                 <label class="block">
@@ -59,14 +61,16 @@
                         Potvrdenie Hesla
                     </span>
                     <input
-                        type="confirm pass"
+                        type="password"
                         name="confpass"
                         class="mt-1 px-3 py-2 bg-background shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:ring-green block w-full rounded-md sm:text-sm focus:ring-1"
                         placeholder=""
+                        v-model="password_confirm"
                     />
                 </label>
                 <button
                     class="bg-green rounded mt-6 mb-2 w-full text-dark font-bold h-9"
+                    @click="checkForm"
                 >
                     Potvrdiť
                 </button>
@@ -76,7 +80,96 @@
 </template>
 
 <script>
+import axios from "axios";
+import { useUserInfo } from "@/stores/userData";
+
+const store = useUserInfo()
 export default {
     name: "RegPage",
-};
+    data() {
+        return {
+            email: null,
+            fullName: null,
+            name: null,
+            surname: null,
+            password: null,
+            password_confirm: null,
+            numOfMissingData: 0,
+            valid_pass: {
+                under20: true,
+                over8: true,
+            }
+        }
+    },
+    methods: {
+        checkForm() {
+            if (this.email === null || this.email === "") {
+                this.numOfMissingData++
+            }
+            if (this.fullName === null || this.fullName === "") {
+                this.numOfMissingData++
+            }
+            if (this.password === null || this.password === "") {
+                this.numOfMissingData++
+            }
+            if (this.password_confirm === null || this.password_confirm === "") {
+                this.numOfMissingData++
+            }
+            if (this.numOfMissingData > 0) {
+                return false
+            }
+            this.numOfMissingData = 0
+            if (this.password.length < 8) {
+                this.valid_pass.over8 = false
+                this.errorCheck()
+                return false
+            } else if (this.password.length > 20) {
+                this.valid_pass.under20 = false
+                this.errorCheck()
+                return false
+            }
+            this.splitName()
+            this.sendData()
+            return false
+        },
+        splitName() {
+            console.log(this.fullName)
+            const names = this.fullName.split(" ")
+            this.name = names[0]
+            this.surname = names[names.length - 1]
+            if (this.name === this.surname) {
+                this.surname = "NevieSvojePriezvisko"
+            }
+        },
+        async sendData() {
+            try {
+                axios({
+                    method: "post",
+                    url: "/auth/signup",
+                    data: {
+                        name: this.name,
+                        surname: this.surname,
+                        email: this.email,
+                        password: this.password,
+                        password_confirm: this.password_confirm
+                    }
+                }).then((response) => {
+                    console.log(response)
+                    store.UserToken = response.data.data.token
+                    store.UserData = response.data.data.user
+                })
+            } catch (errors) {
+                console.log(errors)
+            }
+        },
+        errorCheck() {
+
+        }
+    },
+    computed: {
+        missing() {
+            return Array.from({ length: this.numOfMissingData });
+        },
+    },
+}
 </script>
