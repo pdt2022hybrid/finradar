5fc6e3d9757bd0479106e26ccf30fd26360aaaac
Ivan Polák <ivan.polak25@gmail.com>
2023-05-17 08:31:26 +0200

    [be] fix routes

--- cms/plugins/appentities/company/http/controllers/SearchController.php
+++ cms/plugins/appentities/company/http/controllers/SearchController.php
@@ -3,11 +3,22 @@
 use Illuminate\Routing\Controller;
 use Appentities\Company\Models\Company;
 use AppEntities\Company\Http\Resources\CompanyResource;
+use Illuminate\Support\Facades\Validator;
+use October\Rain\Exception\ValidationException;
 
 class SearchController extends Controller
 {
     public function search()
     {
+        $validator = Validator::make(request()->all(), [
+            'query' => 'required|min:3',
+        ]);
+
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
 
         $textResults = Company::search(input('query'))->get();
         $icoResults = Company::where('ico', 'like', '%'.input('query').'%')->get();
