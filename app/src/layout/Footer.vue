c94ed2fc15edff6d644a510e290340f32f5e1aa7
alegzzz <Alex.Szabo@spsehalova.sk>
2023-06-06 09:29:09 +0200

    kontakt

--- app/src/layout/Footer.vue
+++ app/src/layout/Footer.vue
@@ -4,17 +4,16 @@
             <router-link to="/Pages">
                 <p class="active:text-green">Mapa Stránok</p>
             </router-link>
-            <p class="hover:cursor-pointer active:text-green disabled opacity-60">Developeri</p>
             <div class="flex flex-row">
-                <div v-if="popup" class="bg-white border border-b-searchborder absolute p-2">
-                    <p class="text-dark">
+                <div v-if="popup" class="bg-white border border-b-searchborder absolute p-4 rounded-md">
+                    <p class="text-dark mb-3 font-semibold">
                         Polak@openlab.sk
                     </p>
-                    <p class="text-dark">
+                    <p class="text-dark font-semibold">
                         Vilagi@openlab.sk
                     </p>
                 </div>
-                <p class="hover:cursor-pointer active:text-green disabled opacity-60" @mouseover="popup = true">Kontakt</p>
+                <p class="hover:cursor-pointer active:text-green disabled opacity-60" @click="popup = !popup">Kontakt</p>
             </div>
         </div>
     </div>
